68c844fb7e05867b31f34087de5ce775419b3f80
MichalBartusiak <42718087+MichalBartusiak@users.noreply.github.com>
2020-09-25 12:14:15 +0200

    Fully working table with all data

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Paper, Card } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 const MainContent = () => {
   return (
     <div className="mainContent">
@@ -8,12 +8,8 @@ const MainContent = () => {
       </Typography>
       <Typography variant="body1" color="textSecondary">
         Work App jest aplikacją przeznaczoną dla niewielkich przedsiębiorstw,
-        zatrudniających małą liczbe ludzi. Pomaga ona w zarządzaniu
-        pracownikami, ich stawkami godzinowmi oraz wypłatam, co pozwala na
-        bieżąco śledzić wydatki firmy na pracowników. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Possimus non hic cupiditate dolorum
-        voluptate eligendi doloremque culpa, aperiam asperiores? Incidunt vel
-        tempore hic numquam? Debitis atque aliquid similique placeat obcaecati?
+        zatrudniających małą liczbe ludzi. Pomaga ona na bieżąco śledzić wydatki
+        firmy na pracowników.
       </Typography>
     </div>
   );
--- src/components/Workers/Workers.js
+++ src/components/Workers/Workers.js
@@ -1,8 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import MUIDataTable from "mui-datatables";
+import { TextField, Button } from "@material-ui/core";
 const Workers = () => {
+  const [state, setState] = useState({
+    name: "",
+    surname: "",
+    money: "",
+    hours: "",
+    summary: 0,
+  });
+  const [data, setData] = useState([]);
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setState({ ...state, [e.target.name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, surname, money, hours } = state;
+    setData([
+      ...data,
+      [name, surname, money, hours, (state.summary = money * hours)],
+    ]);
+    setState({ name: "", surname: "", money: "", hours: "", summary: "" });
+  };
+  const columns = ["Imię", "Nazwisko", "zł/h", "Liczba godzin", "Wypłata"];
+  const options = {
+    filterType: "checkbox",
+  };
   return (
     <div className="workers">
-      <h2>Workers</h2>
+      <MUIDataTable
+        title={"Pracownicy"}
+        data={data}
+        columns={columns}
+        options={options}
+      />
+      <TextField
+        name="name"
+        value={state.name}
+        onChange={handleInputChange}
+        label="Name"
+      />
+      <TextField
+        name="surname"
+        value={state.surname}
+        onChange={handleInputChange}
+        label="Surname"
+      />
+      <TextField
+        name="money"
+        value={state.money}
+        onChange={handleInputChange}
+        label="Money"
+      />
+      <TextField
+        name="hours"
+        value={state.hours}
+        onChange={handleInputChange}
+        label="Hours"
+      />
+      <Button onClick={handleSubmit}>Send</Button>
     </div>
   );
 };
